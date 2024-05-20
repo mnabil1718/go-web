b275031c4c9777bf2c9c25cb5df85ea94feb41b4
tingkatin <muhammad.nabil@superindo.co.id>
2024-05-20 15:02:40 +0700

    Added query param implementation

--- http_test.go
+++ http_test.go
@@ -5,11 +5,19 @@ import (
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
+	"strings"
 	"testing"
 )
 
+func SayHelloQueryParam(writer http.ResponseWriter, request *http.Request) {
+	var values url.Values = request.URL.Query()
+	names := values["name"]
+	fmt.Fprintln(writer, strings.Join(names, " "))
+}
+
 func ExampleHandler(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "Hello")
+	fmt.Fprintln(writer, "Hello")
 }
 
 func TestHttp(t *testing.T) {
@@ -26,5 +34,20 @@ func TestHttp(t *testing.T) {
 
 	fmt.Println(response.StatusCode)
 	fmt.Println(string(body))
+}
 
+func TestQueryParam(t *testing.T) {
+	request := httptest.NewRequest("GET", "http://localhost:8080?name=Muhammad&name=Eko", nil)
+	recorder := httptest.NewRecorder()
+
+	SayHelloQueryParam(recorder, request)
+
+	response := recorder.Result()
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(response.StatusCode)
+	fmt.Println(string(body))
 }
