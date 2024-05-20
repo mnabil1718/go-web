c23b78c0e51dcafdecfbd42d029f77976aab930e
tingkatin <muhammad.nabil@superindo.co.id>
2024-05-20 13:27:59 +0700

    Added ServeMux implementation

--- server_test.go
+++ server_test.go
@@ -39,3 +39,34 @@ func TestServerHandler(t *testing.T) {
 	}
 
 }
+
+func TestServeMux(t *testing.T) {
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, you are at %s", r.URL.Path)
+	})
+
+	mux.HandleFunc("/projects", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, you are at projects page")
+	})
+
+	mux.HandleFunc("/about/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, you are at about page. But this path is not prioritized.")
+	})
+
+	mux.HandleFunc("/about/us", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, you are inside a nested about us path. This path is prioritized.")
+	})
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
