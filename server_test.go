f0c7c17a0b8a2581564977cc28113f7121c0e2d1
tingkatin <muhammad.nabil@superindo.co.id>
2024-05-20 11:40:22 +0700

    Added handler func implementation

--- server_test.go
+++ server_test.go
@@ -1,6 +1,7 @@
 package goweb
 
 import (
+	"fmt"
 	"net/http"
 	"testing"
 )
@@ -9,6 +10,27 @@ func TestWebServer(t *testing.T) {
 
 	server := http.Server{
 		Addr: "localhost:8080",
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func TestServerHandler(t *testing.T) {
+
+	var handler http.HandlerFunc = func(writer http.ResponseWriter, request *http.Request) {
+		_, err := fmt.Fprintf(writer, request.RequestURI)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: handler,
 	}
 
 	err := server.ListenAndServe()
